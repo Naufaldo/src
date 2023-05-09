2f4f74c84db0b83a6834908b4780bbba0bd0b3a0
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-09 16:46:50 +0700

    upd

--- sensors/scripts/grip.py
+++ sensors/scripts/grip.py
@@ -9,7 +9,7 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(5)
+    p.ChangeDutyCycle(15)
     time.sleep(0.5)
     p.ChangeDutyCycle(7.5)
     time.sleep(0.5)
