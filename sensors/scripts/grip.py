6a81002bb0c6d98202ab1124b6ed3fb5cad4ebff
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-09 16:38:25 +0700

    upd

--- sensors/scripts/grip.py
+++ sensors/scripts/grip.py
@@ -1,26 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(31,GPIO.OUT)
+servoPIN = 6
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-servo = GPIO.PWM(11,50)
-
-servo.start(0)
-print ("Waiting for 1 second")
-time.sleep(1)
-print ("Rotating at intervals of 12 degrees")
-duty = 2
-while duty <= 17:
-servo.ChangeDutyCycle(duty)
-time.sleep(1)
-duty = duty + 1
-
-print ("Turning back to 0 degrees")
-servo.ChangeDutyCycle(2)
-time.sleep(1)
-servo.ChangeDutyCycle(0)
-
-Servo.stop()
-GPIO.cleanup()
-print ("Everything's cleaned up")
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
