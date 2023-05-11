41d925f2d1fe70c4ddfbf4bcbe0b143a79f03550
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-11 11:02:50 +0700

    updt gripper

---
+++ gripper_subscriber/src/grip2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import rospy
+# from std_msgs.msg import UInt16
+from std_msgs.msg import Bool
+import RPi.GPIO as GPIO
+
+SERVO_PIN = 6
+# SERVO_MIN_DUTY_CYCLE = 15   # 7.3% duty cycle = 0 degrees
+# SERVO_MAX_DUTY_CYCLE = 100  # 14.6% duty cycle = 180 degrees
+
+def servo_callback(msg):
+    # Set the duty cycle for the servo based on the received message
+    # duty_cycle = msg.data
+
+    # Limit the duty cycle to the valid range
+    # duty_cycle = max(duty_cycle, SERVO_MIN_DUTY_CYCLE)
+    # duty_cycle = min(duty_cycle, SERVO_MAX_DUTY_CYCLE)
+
+    # Write the duty cycle to the PWM pin
+    pwm = GPIO.PWM(SERVO_PIN, 50)
+    pwm.start(2.5)
+    if msg.data == True:
+        pwm.ChangeDutyCycle(85)
+    else:
+        pwm.ChangeDutyCycle(25)
+
+    
+
+def main():
+    # Initialize the ROS node
+    rospy.init_node('gripper_subscriber')
+
+    # Set up GPIO pin 6 as a PWM output
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+    # Create a subscriber for the servo position
+    # servo_sub = rospy.Subscriber('servo_position', UInt16, servo_callback)
+    servo_sub = rospy.Subscriber('servo_position', Bool, servo_callback)
+
+    # Spin and wait for callbacks
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
---
+++ gripper_subscriber/src/grippack.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Bool
+import RPi.GPIO as GPIO
+
+# Set the GPIO pin for the servo motor
+servo_pin = 6
+
+# Initialize the GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+# Set up PWM on the GPIO pin for the servo
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(0)
+
+# Create a callback function to handle incoming messages
+def callback(msg):
+    # If the message value is True, set the servo to 180 degrees
+    if msg.data:
+        pwm.ChangeDutyCycle(12.5)
+    # If the message value is False, set the servo to 90 degrees
+    else:
+        pwm.ChangeDutyCycle(7.5)
+
+# Initialize the node
+rospy.init_node('gripper_subscriber_node')
+
+# Create a subscriber with topic name '/gripper_control' and message type Bool
+subscriber = rospy.Subscriber('/gripper_control', Bool, callback)
+
+# Spin the node to receive messages
+rospy.spin()
+
+# Clean up the GPIO pins
+GPIO.cleanup()
