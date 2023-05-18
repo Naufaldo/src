97da2a5a301302be2ee5db429d5bea5a6cac6c1a
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-18 18:15:40 +0700

    update

---
+++ sensors/scripts/grips.py
@@ -0,0 +1,27 @@
+import rospy
+from std_msgs.msg import Bool
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 6
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+
+def handle_toggle(gripper):
+    p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+    p.start(2.5) # Initialization
+    req = gripper.data
+    if req == True:
+        p.ChangeDutyCycle(13)
+    else:
+        p.ChangeDutyCycle(7.5)
+    return []
+
+def listener():
+    rospy.init_node('gripper')
+    s = rospy.Service('gripper', Bool, handle_toggle)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
---
+++ sensors/scripts/listener2.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+import time
+from std_msgs.msg import UInt16
+from std_msgs.msg import Bool
+from std_msgs.msg import UInt16 as a
+#urusan sama GPIO
+kondisi = ""
+ir = 0
+
+def callback_f_servo(flag):
+    kondisi = flag.data
+    print(kondisi)
+
+
+def callback(arduinoData):
+     dataArdiuno = arduinoData.data
+     parsed = dataArdiuno.split(',')
+     parsed = [x.rstrip() for x in parsed]
+     print(parsed)
+     if(len(parsed) > 2):
+        ir = int(int(parsed[0] + '0')/10)
+        ta1 = int(int(parsed[1] + '0')/10)
+        ta2 = int(int(parsed[2] + '0')/10)
+        ta3 = int(int(parsed[3] + '0')/10)
+        ta4 = int(int(parsed[4] + '0')/10)
+        ta5 = int(int(parsed[5] + '0')/10)
+        ta6 = int(int(parsed[6] + '0')/10)
+        ta7 = int(int(parsed[7] + '0')/10)
+        ta8 = int(int(parsed[8] + '0')/10)
+        uvtron = int(int(parsed[9] + '0')/10)
+        print(ir)
+        pub_ir.publish(ir)
+        
+
+def listener():
+    head = rospy.Publisher('head_scalar', String, queue_size=10)
+    pub = rospy.Publisher('servo', UInt16, queue_size=1)
+    pub_pompa = rospy.Publisher('pompa',  UInt16, queue_size=1)
+    rospy.init_node('control_servo', anonymous=False)
+    rospy.Subscriber('chatter', String, callback)
+    rospy.Subscriber('f_servo', String, callback_f_servo)
+    rospy.spin()
+
+def griper():
+    if kondisi == "a":
+        pub.publish(45)
+    elif kondisi == "b":
+        pub.publish(15)
+    elif kondisi == "c":
+        head.publish(45)
+    elif kondisi == "d":
+        head.publish(0)
+    elif kondisi== "e":
+        if ta5 > 47:
+            pub_pompa.publish(True)
+        else:
+            pub_pompa.publish(False)
+    
+            
+    
+    #    if ir <= 3:
+    #        head.publish("45")
+    #        sudut = 15
+    #    else:
+    #        head.publish("0")
+    #        sudut = 45
+    #    pub.publish(sudut)
+	
+
+if __name__ == '__main__':
+    pub_ir = rospy.Publisher('ir', UInt16, queue_size=1)
+    listener()
+    griper()
+
