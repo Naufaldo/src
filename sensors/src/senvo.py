243c12d3564ffc5ed877d4425664316259307b02
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-18 18:57:36 +0700

    update

--- sensors/src/senvo.py
+++ sensors/src/senvo.py
@@ -7,23 +7,25 @@ import time
 
 # Assign GPIO Pins for each sensor
 GPIO_SIG = [17, 18, 19, 20, 21]  # Modify these pins based on your wiring
+#kanan depan , kanan belakang, tengah belakang, kiri belakang, kiri depan
 
 # Set the GPIO pin for the servo motor
 servo_pin = 6
 
 # Initialize the GPIO pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin, GPIO.OUT)
 
-# Set up PWM on the GPIO pin for the servo
-pwm = GPIO.PWM(servo_pin, 50)
-pwm.start(0)
 
 # Create a publisher for the sensor data
 sensor_publisher = rospy.Publisher('sensor_data', Float32MultiArray, queue_size=10)
 
 # Create a callback function to handle incoming servo position messages
 def servo_callback(msg):
+    GPIO.setup(servo_pin, GPIO.OUT)
+
+    # Set up PWM on the GPIO pin for the servo
+    pwm = GPIO.PWM(servo_pin, 50)
+    pwm.start(0)
     # If the message value is True, set the servo to 180 degrees
     if msg.data:
         pwm.ChangeDutyCycle(8)
