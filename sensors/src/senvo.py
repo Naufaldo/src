909ffd39e76dfb3eb4cdc676c541865ab781c072
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-18 18:48:59 +0700

    updt

--- sensors/src/senvo.py
+++ sensors/src/senvo.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Bool, Float32MultiArray
 import RPi.GPIO as GPIO
+import time
 
 # Assign GPIO Pins for each sensor
 GPIO_SIG = [17, 18, 19, 20, 21]  # Modify these pins based on your wiring
@@ -30,7 +31,35 @@ def servo_callback(msg):
 # Create a callback function to handle incoming sensor data messages
 def sensor_callback(msg):
     # Access the sensor data array
-    sensor_data = msg.data
+    sensor_data = []
+
+    for i, pin in enumerate(GPIO_SIG):
+        # Measure distance for each sensor
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
+
+        time.sleep(0.000002)
+
+        # Send trigger signal
+        GPIO.output(pin, 1)
+
+        time.sleep(0.000005)
+
+        GPIO.output(pin, 0)
+
+        GPIO.setup(pin, GPIO.IN)
+
+        while GPIO.input(pin) == 0:
+            starttime = time.time()
+
+        while GPIO.input(pin) == 1:
+            endtime = time.time()
+
+        duration = endtime - starttime
+        # Distance is defined as time/2 (there and back) * speed of sound 34000 cm/s
+        distance = (duration * 34000) / 2
+
+        sensor_data.append(distance)
 
     # Process and use the sensor data as needed
     for i, distance in enumerate(sensor_data):
