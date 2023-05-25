be044146a7a9ddb1879f41d8dbf0649bc7492ccf
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-25 13:41:48 +0700

    upd

--- hexapod_controller/src/control.cpp
+++ hexapod_controller/src/control.cpp
@@ -440,8 +440,8 @@ void Control::bodyCallback(const geometry_msgs::AccelStampedConstPtr &body_scala
 void Control::headCallback(const geometry_msgs::TwistConstPtr &head_Tws_msg)
 {
     
-        head_.yaw = head_Tws_msg->twist.angular.z * HEAD_MAX_YAW;
-        head_.pitch = head_Tws_msg->twist.angular.y * HEAD_MAX_PITCH;
+        head_.yaw = head_Tws_msg->angular.z * HEAD_MAX_YAW;
+        head_.pitch = head_Tws_msg->angular.y * HEAD_MAX_PITCH;
 
         
     
