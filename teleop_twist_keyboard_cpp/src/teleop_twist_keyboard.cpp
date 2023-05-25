e7dd7ab96d8fc32d08002b7ca8b89c3504834f80
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-25 13:50:43 +0700

    udpate key

--- teleop_twist_keyboard_cpp/src/teleop_twist_keyboard.cpp
+++ teleop_twist_keyboard_cpp/src/teleop_twist_keyboard.cpp
@@ -315,8 +315,8 @@ int main(int argc, char **argv)
     body_scalar_.accel.angular.y = ya * turn;
     body_scalar_.accel.angular.z = za * turn;
 
-    head_Tws.linear.x = xb * turn;
-    head_Tws.linear.y = yb * turn * -2;
+    head_Tws.linear.x = xb * turn; //lifter
+    head_Tws.linear.y = yb * turn * -1.7; //gripper
 
     // head_scalar_.header.stamp = current_time;
     // head_scalar_.accel.angular.z = xb * turn;
