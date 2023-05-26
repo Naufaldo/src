7178feb7fd16be54e014091841cf7ddb53a69f4c
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-05-26 20:04:33 +0700

    update

--- navstack_pub/src/Gerakan2.cpp
+++ navstack_pub/src/Gerakan2.cpp
@@ -93,7 +93,7 @@ int gerak_1_[]={0,0,0,0,0,0,0,0,0};
 std::map<int, std::vector<int>> step{
   // {1, {0,0,-2,0,0,0,0,0,0.5,0.5}},   //batas 0-7, speed, turn  //rotate kanan
   // Penejlasan {urutan gerakan , {lmit sensor 1,2,3,4,5 , nilai gripper x , nilai gripper y}}
-  {0, {-2,18,7,52,320,-2 ,0}}
+  {0, {-2,18,7,52,320,-2 ,0}},
   {1, {52,320,7,18,18,-2 ,0}}, // posisi home gerak ke kanan
   {2, {24,320,38,57,57,0,0}},
   {3, {46,320,21,59,60,0,0}},
