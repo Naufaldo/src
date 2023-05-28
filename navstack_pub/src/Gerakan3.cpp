9b63628f28fac82d9b4bec2e52d75db0bf19fa4d
AlfinFF16 <alfin160103@gmail.com>
2023-05-28 19:16:53 +0700

    update

--- navstack_pub/src/Gerakan3.cpp
+++ navstack_pub/src/Gerakan3.cpp
@@ -134,7 +134,6 @@ std::map<int, std::vector<bool>> _f_{
   // ini program untuk kondisi if 1 atau 0 (komparasi)
   // {1, {0,0,1,0,0,0,0,0,0}},  //kompar 0-4 (0)(sensor>=batas) (1)(Sensor<=batas), LaserOrOdom(1=lase && 0=odom) //odom ,imu over , leg height
   //uneven = 0,1 && normal = 0,0 ( 2 digit terakhir) 
-  /
   {0, {0,0,0,0,1,0,1}}, // posisi home gerak ke kanan semua sensor nilai lebih dari batas
   {1, {0,1,1,0,1,0,1}},
   {2, {0,0,0,0,1,0,1}},
