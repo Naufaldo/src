6fdb5907200d99022fe3958f25d7fd2e718e5da4
AlfinFF16 <alfin160103@gmail.com>
2023-05-29 10:02:08 +0700

    update

--- navstack_pub/src/Gerakan3.cpp
+++ navstack_pub/src/Gerakan3.cpp
@@ -88,8 +88,8 @@ std::map<int, std::vector<int>> step{
   // Penejlasan {urutan gerakan , {lmit sensor 1,2,3,4,5 , nilai gripper x , nilai gripper y}}
   // Depan kanan, Belakang Kanan, Belakang, Belakang kiri, depan kiri,lifter , gripper
   //{step, {Tof_Kanan, Tof_depan, Tof_belakang, Tof_Kiri, Gripper, Gripper}}
-  {0, {0, 250, 0, 550, -2 ,0,2,2}},
-  {1, {0, 110, 220, 0, 0, -1,1,2}},
+  {0, {0, 250, 0, 550, -2 ,0,1,1}},
+  {1, {0, 110, 220, 0, 0, -1,1,1}},
   {2, {0, 250, 0, 0, -2, 0,2,2}},
   {3, {0, 0, 100, 0, -2, 0,2,2}},
   {4, {350, 800, 0, 300, -2, 0,2,2}},
