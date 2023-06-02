4c44c696bcbaebb1451d87c85bf5e639cd73e665
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-06-02 14:13:42 +0700

    as

--- Mapping/convert_to_binary.py
+++ Mapping/convert_to_binary.py
@@ -1,7 +1,7 @@
 import cv2 # Import OpenCV
    
 # read the image file
-img = cv2.imread('DenahLapanganKRSRI2023.png')
+img = cv2.imread('D:\Kuliah Online\Tugas Akhir\software Robot\Ancabot\src\Mapping\DenahLapanganKRSRI2023.png')
    
 ret, bw_img = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)
    
@@ -10,6 +10,6 @@ bw = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
   
 # Display and save image 
 cv2.imshow("Binary", bw_img)
-cv2.imwrite("DenahLapanganKRSRI2023BW.png", bw_img)
+cv2.imwrite("D:\Kuliah Online\Tugas Akhir\software Robot\Ancabot\src\Mapping\DenahLapanganKRSRI2023BW.png", bw_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
