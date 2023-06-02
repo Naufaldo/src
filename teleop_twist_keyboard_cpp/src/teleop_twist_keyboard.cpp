5b7f7c7bbc0bbc891d69b33f524c7a862b3b00fd
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-06-02 11:03:40 +0700

    upd

--- teleop_twist_keyboard_cpp/src/teleop_twist_keyboard.cpp
+++ teleop_twist_keyboard_cpp/src/teleop_twist_keyboard.cpp
@@ -316,7 +316,7 @@ int main(int argc, char **argv)
     body_scalar_.accel.angular.z = za * turn;
 
     head_Tws.linear.x = xb * 0.7; //lifter
-    head_Tws.linear.y = yb * turn ; //gripper
+    head_Tws.linear.y = yb * 1 ; //gripper
 
     // head_scalar_.header.stamp = current_time;
     // head_scalar_.accel.angular.z = xb * turn;
