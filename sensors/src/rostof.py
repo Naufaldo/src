ca73bc26c19db853c198a562dae3ec2e11d34df7
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-06-12 11:41:26 +0700

    upd

--- sensors/src/rostof.py
+++ sensors/src/rostof.py
@@ -101,9 +101,7 @@ GPIO.output(sensor8_shutdown, GPIO.HIGH)
 time.sleep(0.50)
 tof7.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
 
-timing = max(tof.get_timing(), tof1.get_timing(), tof2.get_timing(), tof3.get_timing(),
-                     tof4.get_timing(), tof5.get_timing(), tof6.get_timing(), tof7.get_timing())
-# timing = tof.get_timing()
+timing = tof.get_timing()
 if timing < 100000:
     timing = 100000
 print("Timing %d ms" % (timing / 1000))
