44f7b79c670878afcd64a83e019f858b0c4a433b
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-06-22 19:30:27 +0700

    as

--- sensors/src/rostof.py
+++ sensors/src/rostof.py
@@ -7,10 +7,63 @@ import VL53L0X
 import rospy
 from std_msgs.msg import Int32MultiArray
 
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Import the required libraries for the GME12864 display using I2C
+import Adafruit_GPIO.I2C as I2C
+import Adafruit_SSD1306
+
+# Define display constants
+DISPLAY_WIDTH = 128
+DISPLAY_HEIGHT = 64
+
+# Initialize the display using I2C with the specified parameters
+RST = None  # If you have a reset pin, specify it here
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+disp.begin()
+disp.clear()
+disp.display()
+
+# Create a blank image for drawing
+image = Image.new('1', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+draw = ImageDraw.Draw(image)
+
+# Define font
+font = ImageFont.load_default()
+
 def publish_distances(distances):
     # Publish the distances as Int32MultiArray
     msg = Int32MultiArray(data=distances)
     pub.publish(msg)
+
+def display_distances(sensor_distances):
+    # Clear the image
+    draw.rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=0)
+
+    # Display the sensor array
+    array_text = 'Sensor Array:\n{}'.format(sensor_distances)
+    draw.text((0, 0), array_text, font=font, fill=255)
+
+    # Display the image
+    disp.image(image)
+    disp.display()
+
+def display_error(sensor_index):
+    # Calculate the top position for displaying the error message
+    top = DISPLAY_HEIGHT - 20
+
+    # Clear the image
+    draw.rectangle((0, top, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=0)
+
+    # Display the sensor index and error message
+    draw.text((0, top), 'Sensor {}:'.format(sensor_index), font=font, fill=255)
+    draw.text((0, top + 20), 'Error', font=font, fill=255)
+
+    # Display the image
+    disp.image(image)
+    disp.display()
 
 rospy.init_node('tof_publisher', anonymous=True)
 pub = rospy.Publisher('tof_distances', Int32MultiArray, queue_size=10)
@@ -32,29 +85,40 @@ print("Timing %d ms" % (timing / 1000))
 
 try:
     while not rospy.is_shutdown():
-        distances = []
+        sensor_distances = []
+        has_error = False
 
         # Get distances from each sensor
         for i, sensor in enumerate(sensors):
             try:
                 distance = sensor.get_distance()
                 if distance > 0:
-                    distances.append(distance)
+                    sensor_distances.append(distance)
                 else:
                     print("Error: Invalid distance value from sensor %d" % i)
+                    has_error = True
             except Exception as e:
                 print("Error: Failed to get distance from sensor %d. Exception: %s" % (i, str(e)))
+                has_error = True
 
-        # Check if all 8 sensors have published their distances
-        if len(distances) == 4:
-            publish_distances(distances)
+        # Display the distances if no error occurred
+        if not has_error:
+            display_distances(sensor_distances)
+        else:
+            # Display sensor distances before showing the error message
+            display_distances(sensor_distances)
+            display_error(len(sensors))
+
+        # Check if all sensors have published their distances
+        if len(sensor_distances) == len(sensors):
+            publish_distances(sensor_distances)
         else:
             print("Error: Failed to get distances from all sensors")
-            publish_distances(distances)
+            publish_distances(sensor_distances)
 
         time.sleep(timing / 1000000.00)
 except KeyboardInterrupt:
-    # Stop ranging for all sensors when program is interrupted
+    # Stop ranging for all sensors when the program is interrupted
     for sensor in sensors:
         sensor.stop_ranging()
 
