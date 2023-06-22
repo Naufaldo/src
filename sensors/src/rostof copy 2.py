31e43a9f1e3bf24ec4c291ce5fd8ab6860c6b4b1
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-06-22 17:52:28 +0700

    as

--- sensors/src/rostof copy 2.py
+++ sensors/src/rostof copy 2.py
@@ -4,180 +4,141 @@ import time
 import sys
 sys.path.append('/home/pi/VL53L0X_rasp_python/python')
 import VL53L0X
-import RPi.GPIO as GPIO
 import rospy
 from std_msgs.msg import Int32MultiArray
+from sensor_msgs.msg import Imu
 
-# GPIO for Sensor 1 shutdown pin
-sensor1_shutdown = 4
-# GPIO for Sensor 2 shutdown pin
-sensor2_shutdown = 17
-# GPIO for Sensor 3 shutdown pin
-sensor3_shutdown = 27
-# GPIO for Sensor 4 shutdown pin
-sensor4_shutdown = 22
-# GPIO for Sensor 5 shutdown pin
-sensor5_shutdown = 12
-# GPIO for Sensor 6 shutdown pin
-sensor6_shutdown = 16
-# GPIO for Sensor 7 shutdown pin
-sensor7_shutdown = 20
-# GPIO for Sensor 8 shutdown pin
-sensor8_shutdown = 21
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
-GPIO.setwarnings(False)
+# Import the required libraries for the GME12864 display using I2C
+import Adafruit_GPIO.I2C as I2C
+import Adafruit_SSD1306
 
-# Setup GPIO for shutdown pins on each VL53L0X
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(sensor1_shutdown, GPIO.OUT)
-GPIO.setup(sensor2_shutdown, GPIO.OUT)
-GPIO.setup(sensor3_shutdown, GPIO.OUT)
-GPIO.setup(sensor4_shutdown, GPIO.OUT)
-GPIO.setup(sensor5_shutdown, GPIO.OUT)
-GPIO.setup(sensor6_shutdown, GPIO.OUT)
-GPIO.setup(sensor7_shutdown, GPIO.OUT)
-GPIO.setup(sensor8_shutdown, GPIO.OUT)
+# Define display constants
+DISPLAY_WIDTH = 128
+DISPLAY_HEIGHT = 64
 
-# Set all shutdown pins low to turn off each VL53L0X
-GPIO.output(sensor1_shutdown, GPIO.LOW)
-GPIO.output(sensor2_shutdown, GPIO.LOW)
-GPIO.output(sensor3_shutdown, GPIO.LOW)
-GPIO.output(sensor4_shutdown, GPIO.LOW)
-GPIO.output(sensor5_shutdown, GPIO.LOW)
-GPIO.output(sensor6_shutdown, GPIO.LOW)
-GPIO.output(sensor7_shutdown, GPIO.LOW)
-GPIO.output(sensor8_shutdown, GPIO.LOW)
+# Initialize the display using I2C with the specified parameters
+RST = None  # If you have a reset pin, specify it here
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+disp.begin()
+disp.clear()
+disp.display()
 
-# Keep all low for 500 ms or so to make sure they reset
-time.sleep(0.50)
+# Create a blank image for drawing
+image = Image.new('1', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+draw = ImageDraw.Draw(image)
 
-# Create one object per VL53L0X passing the address to give to each
-tof = VL53L0X.VL53L0X(address=0x2B)
-tof1 = VL53L0X.VL53L0X(address=0x2C)
-tof2 = VL53L0X.VL53L0X(address=0x2F)
-tof3 = VL53L0X.VL53L0X(address=0x2E)
-tof4 = VL53L0X.VL53L0X(address=0x30)
-tof5 = VL53L0X.VL53L0X(address=0x31)
-tof6 = VL53L0X.VL53L0X(address=0x32)
-tof7 = VL53L0X.VL53L0X(address=0x33)
+# Define font
+font = ImageFont.load_default()
 
-# Set shutdown pin high for the first VL53L0X, then call to start ranging
-GPIO.output(sensor1_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+def publish_distances(distances):
+    # Publish the distances as Int32MultiArray
+    msg = Int32MultiArray(data=distances)
+    pub.publish(msg)
 
-# Set shutdown pin high for the second VL53L0X, then call to start ranging
-GPIO.output(sensor2_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof1.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+def display_distances(sensor_distances, orientation_z):
+    # Clear the image
+    draw.rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=0)
 
-# Set shutdown pin high for the third VL53L0X, then call to start ranging
-GPIO.output(sensor3_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof2.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+    # Display the sensor array
+    array_text = 'Sensor Array:\n{}'.format(sensor_distances)
+    draw.text((0, 0), array_text, font=font, fill=255)
 
-# Set shutdown pin high for the fourth VL53L0X, then call to start ranging
-GPIO.output(sensor4_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof3.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+    # Display the orientation Z
+    orientation_text = 'Orientation Z: {}'.format(orientation_z)
+    draw.text((0, 30), orientation_text, font=font, fill=255)
 
-# Set shutdown pin high for the fifth VL53L0X, then call to start ranging
-GPIO.output(sensor5_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof4.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+    # Display the image
+    disp.image(image)
+    disp.display()
 
-# Set shutdown pin high for the sixth VL53L0X, then call to start ranging
-GPIO.output(sensor6_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof5.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+def display_error(sensor_index):
+    # Calculate the top position for displaying the error message
+    top = DISPLAY_HEIGHT - 20
 
-# Set shutdown pin high for the seventh VL53L0X, then call to start ranging
-GPIO.output(sensor7_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof6.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+    # Clear the image
+    draw.rectangle((0, top, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=0)
 
-# Set shutdown pin high for the eighth VL53L0X, then call to start ranging
-GPIO.output(sensor8_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof7.start_ranging(VL53L0X.VL53L0X_HIGH_SPEED_MODE)
+    # Display the sensor index and error message
+    draw.text((0, top), 'Sensor {}:'.format(sensor_index), font=font, fill=255)
+    draw.text((0, top + 20), 'Error', font=font, fill=255)
 
-timing = tof.get_timing()
-if timing < 20000:
-    timing = 20000
-print("Timing %d ms" % (timing / 1000))
+    # Display the image
+    disp.image(image)
+    disp.display()
+
+def imu_callback(data):
+    # Extract the orientation data (z) from the IMU message
+    orientation_z = data.orientation.z
+
+    # Display the distances and orientation Z
+    display_distances(sensor_distances, orientation_z)
 
 rospy.init_node('tof_publisher', anonymous=True)
 pub = rospy.Publisher('tof_distances', Int32MultiArray, queue_size=10)
 
+# Subscribe to the IMU data topic
+rospy.Subscriber('imu/data', Imu, imu_callback)
+
+# Create a list to store the VL53L0X objects for each sensor
+sensors = []
+
+# Create VL53L0X objects for devices on TCA9548A buses 0 to 7
+for i in range(4):
+    sensor = VL53L0X.VL53L0X(TCA9548A_Num=i, TCA9548A_Addr=0x70)
+    sensor.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+    sensors.append(sensor)
+
+# Get the timing from the first sensor
+timing = sensors[0].get_timing()
+if timing < 100000:
+    timing = 100000
+print("Timing %d ms" % (timing / 1000))
+
 try:
     while not rospy.is_shutdown():
-        # Read distance from each sensor
-        distances = []
+        sensor_distances = []
+        has_error = False
 
-        distance = tof.get_distance()
-        if distance > 0:
-            distances.append(distance)
+        # Get distances from each sensor
+        for i, sensor in enumerate(sensors):
+            try:
+                distance = sensor.get_distance()
+                if distance > 0:
+                    sensor_distances.append(distance)
+                else:
+                    print("Error: Invalid distance value from sensor %d" % i)
+                    has_error = True
+            except Exception as e:
+                print("Error: Failed to get distance from sensor %d. Exception: %s" % (i, str(e)))
+                has_error = True
 
-        distance = tof1.get_distance()
-        if distance > 0:
-            distances.append(distance)
+        # Display the distances if no error occurred
+        if not has_error:
+            display_distances(sensor_distances, 0)  # Provide a default value for orientation Z
+        else:
+            # Display sensor distances before showing the error message
+            display_distances(sensor_distances, 0)  # Provide a default value for orientation Z
+            display_error(len(sensors))
 
-        distance = tof2.get_distance()
-        if distance > 0:
-            distances.append(distance)
-
-        distance = tof3.get_distance()
-        if distance > 0:
-            distances.append(distance)
-
-        distance = tof4.get_distance()
-        if distance > 0:
-            distances.append(distance)
-
-        distance = tof5.get_distance()
-        if distance > 0:
-            distances.append(distance)
-
-        distance = tof6.get_distance()
-        if distance > 0:
-            distances.append(distance)
-
-        distance = tof7.get_distance()
-        if distance > 0:
-            distances.append(distance)
-
-        # Publish the distances as Int32MultiArray
-        msg = Int32MultiArray(data=distances)
-        pub.publish(msg)
-
-        # Get the maximum timing value among all sensors
-        timing = max(tof.get_timing(), tof1.get_timing(), tof2.get_timing(), tof3.get_timing(),
-                     tof4.get_timing(), tof5.get_timing(), tof6.get_timing(), tof7.get_timing())
+        # Check if all sensors have published their distances
+        if len(sensor_distances) == len(sensors):
+            publish_distances(sensor_distances)
+        else:
+            print("Error: Failed to get distances from all sensors")
+            publish_distances(sensor_distances)
 
         time.sleep(timing / 1000000.00)
-
 except KeyboardInterrupt:
-    # Stop ranging and set shutdown pins low when program is interrupted
-    tof.stop_ranging()
-    GPIO.output(sensor1_shutdown, GPIO.LOW)
+    # Stop ranging for all sensors when the program is interrupted
+    for sensor in sensors:
+        sensor.stop_ranging()
 
-    tof1.stop_ranging()
-    GPIO.output(sensor2_shutdown, GPIO.LOW)
+    rospy.loginfo("Program interrupted")
 
-    tof2.stop_ranging()
-    GPIO.output(sensor3_shutdown, GPIO.LOW)
-
-    tof3.stop_ranging()
-    GPIO.output(sensor4_shutdown, GPIO.LOW)
-
-    tof4.stop_ranging()
-    GPIO.output(sensor5_shutdown, GPIO.LOW)
-
-    tof5.stop_ranging()
-    GPIO.output(sensor6_shutdown, GPIO.LOW)
-
-    tof6.stop_ranging()
-    GPIO.output(sensor7_shutdown, GPIO.LOW)
-
-    tof7.stop_ranging()
-    GPIO.output(sensor8_shutdown, GPIO.LOW)
+# Stop ranging for all sensors before exiting
+for sensor in sensors:
+    sensor.stop_ranging()
