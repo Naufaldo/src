ad7352cd02a03912934ec6c8349d0d57770fdba8
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-06-22 17:26:15 +0700

    update display imu_data

--- sensors/src/rostof.py
+++ sensors/src/rostof.py
@@ -6,6 +6,7 @@ sys.path.append('/home/pi/VL53L0X_rasp_python/python')
 import VL53L0X
 import rospy
 from std_msgs.msg import Int32MultiArray
+from sensor_msgs.msg import Imu
 
 from PIL import Image
 from PIL import ImageDraw
@@ -38,13 +39,17 @@ def publish_distances(distances):
     msg = Int32MultiArray(data=distances)
     pub.publish(msg)
 
-def display_distances(sensor_distances):
+def display_distances(sensor_distances, orientation_z):
     # Clear the image
     draw.rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=0)
 
     # Display the sensor array
     array_text = 'Sensor Array:\n{}'.format(sensor_distances)
     draw.text((0, 0), array_text, font=font, fill=255)
+
+    # Display the orientation Z
+    orientation_text = 'Orientation Z: {}'.format(orientation_z)
+    draw.text((0, 30), orientation_text, font=font, fill=255)
 
     # Display the image
     disp.image(image)
@@ -65,8 +70,18 @@ def display_error(sensor_index):
     disp.image(image)
     disp.display()
 
+def imu_callback(data):
+    # Extract the orientation data (z) from the IMU message
+    orientation_z = data.orientation.z
+
+    # Display the distances and orientation Z
+    display_distances(sensor_distances, orientation_z)
+
 rospy.init_node('tof_publisher', anonymous=True)
 pub = rospy.Publisher('tof_distances', Int32MultiArray, queue_size=10)
+
+# Subscribe to the IMU data topic
+rospy.Subscriber('imu/data', Imu, imu_callback)
 
 # Create a list to store the VL53L0X objects for each sensor
 sensors = []
@@ -103,10 +118,10 @@ try:
 
         # Display the distances if no error occurred
         if not has_error:
-            display_distances(sensor_distances)
+            display_distances(sensor_distances, 0)  # Provide a default value for orientation Z
         else:
             # Display sensor distances before showing the error message
-            display_distances(sensor_distances)
+            display_distances(sensor_distances, 0)  # Provide a default value for orientation Z
             display_error(len(sensors))
 
         # Check if all sensors have published their distances
