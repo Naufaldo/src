2190e33eac0f8cb793aac0e15cd49f9aa2b7bc72
AlfinFF16 <alfin160103@gmail.com>
2023-06-23 15:52:50 +0700

    update

--- navstack_pub/src/Gerakan9.cpp
+++ navstack_pub/src/Gerakan9.cpp
@@ -161,7 +161,7 @@ std::map<int, std::vector<bool>> _f_{
 
   // {1, {0,0,1,0,0,0,0,0,0}},  //kompar 0-6 (0)(sensor>=batas) (1)(Sensor<=batas), LaserOrOdom(1=laser && 0=odom) //odom ,imu over , leg height
   //uneven = 0,1 && normal = 0,0 ( 2 digit terakhir) 
-  {0, {0,0,0,1,1,0,1,0,0}},
+  {0, {1,0,0,0,1,0,1,0,0}},
   {1, {0,0,1,0,0,0,1,0,0}},
   {2, {0,0,0,0,0,0,1,0,0}}, // posisi home gerak ke kanan semua sensor nilai lebih dari batas
   {3, {0,0,0,0,0,0,1,0,0}},
