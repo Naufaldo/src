7c883729d2456828f31ab147751dda823d436cf5
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-07-02 10:50:08 +0700

    as

--- sensors/src/Lidar.py
+++ sensors/src/Lidar.py
@@ -4,7 +4,11 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-    print(msg.ranges[360])
+    print('0:', msg.ranges[0])
+    print('90:', msg.ranges[90])
+    print('180:', msg.ranges[180])
+    print('270:', msg.ranges[270])
+    print('360:', msg.ranges[360])
 
 rospy.init_node('data_lidar')
 sub = rospy.Subscriber('/scan', LaserScan, callback)
