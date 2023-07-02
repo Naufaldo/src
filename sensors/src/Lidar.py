abcb6e6dde57e078c9e60274f544a2ddcf90b463
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-07-02 13:38:19 +0700

    as

--- sensors/src/Lidar.py
+++ sensors/src/Lidar.py
@@ -9,7 +9,9 @@ def callback(msg):
     print('180:', msg.ranges[180])
     print('270:', msg.ranges[270])
     print('360:', msg.ranges[360])
+    print('450:', msg.ranges[450])
     print('540:', msg.ranges[540])
+    print('630:', msg.ranges[630])
 
 
 rospy.init_node('data_lidar')
