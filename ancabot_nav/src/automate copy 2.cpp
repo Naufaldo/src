f27c5aa9d2ecdbfc79448ed645e4202c25a5f416
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-07-26 09:57:19 +0700

    update program

--- ancabot_nav/src/automate copy 2.cpp
+++ ancabot_nav/src/automate copy 2.cpp
@@ -25,12 +25,21 @@ int main(int argc, char** argv) {
   // Create the initial pose message
   geometry_msgs::PoseWithCovarianceStamped initialPoseMsg;
   initialPoseMsg.header.frame_id = "map";
-  initialPoseMsg.pose.pose.position.x = 0.0;
-  initialPoseMsg.pose.pose.position.y = 0.0;
+  initialPoseMsg.pose.pose.position.x = 0.029102548956871033;
+  initialPoseMsg.pose.pose.position.y = -0.006215959787368774;
   initialPoseMsg.pose.pose.position.z = 0.0;
   initialPoseMsg.pose.pose.orientation.x = 0.0;
   initialPoseMsg.pose.pose.orientation.y = 0.0;
-  initialPoseMsg.pose.pose.orientation.w = 1;
+  initialPoseMsg.pose.pose.orientation.z = 0.028834942647973153;
+  initialPoseMsg.pose.pose.orientation.w = 0.9995841865908485;
+  initialPoseMsg.pose.covariance = {
+    0.25, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.25, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.06853892326654787
+  };
 
   // Publish the initial pose
   initialPosePub.publish(initialPoseMsg);
@@ -45,21 +54,71 @@ int main(int argc, char** argv) {
 
   // Define the sequence of destinations
   double destinations[][6] = {
+/*
+      seq: 0
+  stamp: 
+    secs: 1690269012
+    nsecs: 432677040
+  frame_id: "map"
+pose: 
+  position: 
+    x: 0.48933225870132446
+    y: 0.006300929468125105
+    z: 0.0
+  orientation: 
+    x: 0.0
+    y: 0.0
+    z: 0.006688636727631092
+    w: 0.0
 
+    ---
+    header: 
+  seq: 1
+  stamp: 
+    secs: 1690269411
+    nsecs: 432564649
+  frame_id: "map"
+pose: 
+  position: 
+    x: 2.384226083755493
+    y: 0.010871904902160168
+    z: 0.0
+  orientation: 
+    x: 0.0
+    y: 0.0
+    z: -0.5791836977005006
+    w: 0.0
+
+---
+header: 
+  seq: 2
+  stamp: 
+    secs: 1690269669
+    nsecs: 931631464
+  frame_id: "map"
+pose: 
+  position: 
+    x: 2.374901533126831
+    y: 0.7160121202468872
+    z: 0.0
+  orientation: 
+    x: 0.0
+    y: 0.0
+    z: 1.4213865995407104
+    w: 0.0
+---
+
+
+*/
     // {x, y, w, Lifter, Gripper, leg_height}
-    {0.46,0.0, 0.0, -2, 0, 0}, // Arah Korban 1
-    {0.46, 0.0,1.5, -2, 0, 0}, // Arah Korban 1
-    {0.46,0.1,1.5, 0, -1, 1},    // Ambil Korban 1
-    {0.46,0.0,0.0, -2, 0, 1},    // Siap Pergi ke Target 2
-    {2.4,0.0,1, -2, 0, 1},    // Pergi ke Target 2
-    {2.4,0.0,-0.6, 0, -1, 1},    // Lepas korban di Target 2
-    {2.4,0.0,1.5, -2, 0, 1},    // Jalan ke Target 3
-    {2.4,0.7,1.5, -2, 0, 1},    // Jalan ke Target 3
-    // {2.4658145904541016, 0.19893264770507812, 0.9196588897926539, -2, 0, 1},   // Simpen Korban 1
-    // {2.1826889514923096, 0.6185263991355896, 0.03423934984405933, -2, 0, 1},    // Arah Korban 2
-    // // Add more destinations in the sequence
-    // {2.4959347248077393, 0.8430209755897522, 0.7359457450143778, -2, 0, 1},     // Simpen korban kelereng
-    // {1.3070660829544067, 0.6406080722808838, 0.01420421912936522, -2, 0, 1}     // Arah Tangga
+    {0.46674978733062744, 0.009915530681610107, 0.9995841865908485, -2, 0, 0}, // Arah Korban 1
+    {0.46674978733062744, 0.009915530681610107, 0.6555718605526236, -2, 0, 0}, // Arah Korban 1
+    {0.47411449790000916, 0.1285257339477539, 0.6555718605526236, 0, -1, 1},    // Ambil Korban 1
+    {2.4658145904541016, 0.19893264770507812, 0.9196588897926539, -2, 0, 1},   // Simpen Korban 1
+    {2.1826889514923096, 0.6185263991355896, 0.03423934984405933, -2, 0, 1},    // Arah Korban 2
+    // Add more destinations in the sequence
+    {2.4959347248077393, 0.8430209755897522, 0.7359457450143778, -2, 0, 1},     // Simpen korban kelereng
+    {1.3070660829544067, 0.6406080722808838, 0.01420421912936522, -2, 0, 1}     // Arah Tangga
   };
   int numDestinations = sizeof(destinations) / sizeof(destinations[0]);
 
@@ -72,7 +131,7 @@ int main(int argc, char** argv) {
     goal.target_pose.header.stamp = ros::Time::now();
     goal.target_pose.pose.position.x = destinations[i][0];
     goal.target_pose.pose.position.y = destinations[i][1];
-    goal.target_pose.pose.orientation.z = destinations[i][2];
+    goal.target_pose.pose.orientation.w = destinations[i][2];
 
     // Create and set the head and leg height messages
     geometry_msgs::Twist headTws;
