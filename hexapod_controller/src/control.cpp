a248e9cbbe787a17bc43cb53fcc68fa9eaf8b7e8
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-08-08 14:53:37 +0700

    as

--- hexapod_controller/src/control.cpp
+++ hexapod_controller/src/control.cpp
@@ -76,7 +76,7 @@ Control::Control(void)
     // imu_sub_ = nh_.subscribe<sensor_msgs::Imu>("/imu/data", 1, &Control::imuCallback, this);
     //subInitialPose = nh_.subscribe<geometry_msgs::PoseStamped>("/initial_2d", 1, &Control::set_initial_2d, this);
     //sub = n.subscribe("/tld_tracked_object", 20, &callback);
-    subInitialPose = nh_.subscribe("initial_2d", 1, &Control::set_initial_2d, this);
+    subInitialPose = nh_.subscribe("/initial_2d", 1, &Control::set_initial_2d, this);
     // Topics we are publishing
     sounds_pub_ = nh_.advertise<hexapod_msgs::Sounds>("/sounds", 10);
     joint_state_pub_ = nh_.advertise<sensor_msgs::JointState>("/joint_states", 10);
@@ -93,10 +93,10 @@ Control::Control(void)
 
 void Control::set_initial_2d(const geometry_msgs::PoseStamped &rvizClick)
 {
-    initialPose.position.x = rvizClick.pose.position.x;
-    initialPose.position.y = rvizClick.pose.position.y;
-    initialPose.orientation.z = rvizClick.pose.orientation.z;
-    initialPoseReceived = true;
+    odomOld.pose.pose.position.x = rvizClick.pose.position.x;
+    odomOld.pose.pose.position.y = rvizClick.pose.position.y;
+    odomOld.pose.pose.orientation.z = rvizClick.pose.orientation.z;
+    initialPoseRecieved = true;
 }
 
 //==============================================================================
@@ -128,62 +128,113 @@ bool Control::getPrevHexActiveState(void)
 //==============================================================================
 void Control::publishOdometry(const geometry_msgs::Twist &gait_vel)
 {
+    // compute odometry in a typical way given the velocities of the robot
+
     // calculate time elapsed
     current_time_odometry_ = ros::Time::now();
-    double dt = ( current_time_odometry_ - last_time_odometry_ ).toSec();
+    double dt = (current_time_odometry_ - last_time_odometry_).toSec();
 
-    double vth = gait_vel.angular.z;
+    double vth = gait_vel.angular.z * kali_A;
     double delta_th = vth * dt;
     pose_th_ += delta_th;
 
-    double vx = gait_vel.linear.x;
-    double vy = gait_vel.linear.y;
-    double delta_x = ( vx * cos( pose_th_ ) - vy * sin( pose_th_ ) ) * dt;
-    double delta_y = ( vx * sin( pose_th_ ) + vy * cos( pose_th_ ) ) * dt;
+    odomNew.pose.pose.orientation.z = delta_th + odomOld.pose.pose.orientation.z;
+
+    double vx = gait_vel.linear.x * kali_L;
+    double vy = gait_vel.linear.y * kali_L;
+    double delta_x = (vx * cos(pose_th_) - vy * sin(pose_th_)) * dt;
+    double delta_y = (vx * sin(pose_th_) + vy * cos(pose_th_)) * dt;
+    // double delta_x = (vx * cos(odomNew.pose.pose.orientation.z) - vy * sin(odomNew.pose.pose.orientation.z)) * dt;
+    // double delta_y = (vx * sin(odomNew.pose.pose.orientation.z) + vy * cos(odomNew.pose.pose.orientation.z)) * dt;
+    // double delta_x = vx * dt;
+    // double delta_y = vy  * dt;
     pose_x_ += delta_x;
     pose_y_ += delta_y;
+    pose_th_ += delta_th;
+    // msg.data = odom.pose.pose.orientation.x;
+    
 
-    // if (initialPoseReceived) {
-    //         pose_x_ += initialPose.position.x;
-    //         pose_y_ += initialPose.position.y;
-    //         pose_th_ += tf::getYaw(initialPose.orientation);
-    //         initialPoseReceived = false; // Reset the flag
-    //     }
-    // since all odometry is 6DOF we'll need a quaternion created from yaw
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw( pose_th_ );
+    //calculate
+    odomNew.pose.pose.position.x = odomOld.pose.pose.position.x + delta_x;
+    odomNew.pose.pose.position.y = odomOld.pose.pose.position.y + delta_y;
+    odomNew.pose.pose.orientation.z = delta_th + odomOld.pose.pose.orientation.z;
+
+    msg1.data=pose_x_;
+    msg2.data=pose_y_;
+    msg3.data=pose_th_;
+    chatter_pub1.publish(msg1);
+    chatter_pub2.publish(msg2);
+    chatter_pub3.publish(msg3);
+
+    //Prevent lockup from a single bad cycle
+    if (isnan(odomNew.pose.pose.position.x) || isnan(odomNew.pose.pose.position.y) || isnan(odomNew.pose.pose.position.z))
+    {
+        odomNew.pose.pose.position.x = odomOld.pose.pose.position.x;
+        odomNew.pose.pose.position.y = odomOld.pose.pose.position.y;
+        odomNew.pose.pose.orientation.z = odomOld.pose.pose.orientation.z;
+    }
+    //Make sure theta stays in the correct range
+    if (odomNew.pose.pose.orientation.z > PI2)
+    {
+    odomNew.pose.pose.orientation.z = odomNew.pose.pose.orientation.z;
+    }
+    else if (odomNew.pose.pose.orientation.z < -PI2)
+    {
+    odomNew.pose.pose.orientation.z = odomNew.pose.pose.orientation.z;
+    }
+
+    // Save the pose data for the next cycle
+    odomOld.pose.pose.position.x = odomNew.pose.pose.position.x;
+    odomOld.pose.pose.position.y = odomNew.pose.pose.position.y;
+    odomOld.pose.pose.orientation.z = odomNew.pose.pose.orientation.z;
+    odomOld.header.stamp = odomNew.header.stamp;
+
+
+    tf2::Quaternion q;
+         
+    q.setRPY(0, 0, odomNew.pose.pose.orientation.z);
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(odomNew.pose.pose.orientation.z);
+
+    
 
     // first, we'll publish the transform over tf
+
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = current_time_odometry_;
     odom_trans.header.frame_id = "odom";
     odom_trans.child_frame_id = "base_link";
 
-    odom_trans.transform.translation.x = pose_x_;
-    odom_trans.transform.translation.y = pose_y_;
+    odom_trans.transform.translation.x = odomNew.pose.pose.position.x;
+    odom_trans.transform.translation.y = odomNew.pose.pose.position.y;
     odom_trans.transform.translation.z = body_.position.z;
+    
+
+    
     odom_trans.transform.rotation = odom_quat;
+    
 
-    // Uncomment odom_broadcaster to send the transform. Only used if debugging calculated odometry.
-    // odom_broadcaster.sendTransform( odom_trans );
-
-    // next, we'll publish the odometry message over ROS
     nav_msgs::Odometry odom;
     odom.header.stamp = current_time_odometry_;
     odom.header.frame_id = "odom";
     odom.child_frame_id = "base_link";
 
     // set the position
-    odom.pose.pose.position.x = pose_x_;
-    odom.pose.pose.position.y = pose_y_;
+    odom.pose.pose.position.x = odomNew.pose.pose.position.x;
+    odom.pose.pose.position.y = odomNew.pose.pose.position.y;
     odom.pose.pose.position.z = body_.position.z;
     odom.pose.pose.orientation = odom_quat;
+    
+    // odom.pose.pose.orientation.x = q.x();
+    // odom.pose.pose.orientation.y = q.y();
+    // odom.pose.pose.orientation.z = odomNew.pose.pose.orientation.z; //q.z();
+    // odom.pose.pose.orientation.w = q.w();
 
-    odom.pose.covariance[0] = 0.00001;  // x
-    odom.pose.covariance[7] = 0.00001;  // y
-    odom.pose.covariance[14] = 0.00001; // z
+    odom.pose.covariance[0] = 0.00001;          // x
+    odom.pose.covariance[7] = 0.00001;          // y
+    odom.pose.covariance[14] = 0.00001;         // z
     odom.pose.covariance[21] = 1000000000000.0; // rot x
     odom.pose.covariance[28] = 1000000000000.0; // rot y
-    odom.pose.covariance[35] = 0.001; // rot z
+    odom.pose.covariance[35] = 0.001;           // rot z
 
     // set the velocity
     odom.twist.twist.linear.x = vx;
@@ -191,9 +242,11 @@ void Control::publishOdometry(const geometry_msgs::Twist &gait_vel)
     odom.twist.twist.angular.z = vth;
     odom.twist.covariance = odom.pose.covariance; // needed?
 
-    odom_pub_.publish( odom );
+    odom_pub_.publish(odom);
     last_time_odometry_ = current_time_odometry_;
+
 }
+
 
 
 //==============================================================================
