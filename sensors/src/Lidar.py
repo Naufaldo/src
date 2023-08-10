8ec75cc1f68ed4b9484e5b1be9b2a30494eb8951
Naufaldo <60534884+Naufaldo@users.noreply.github.com>
2023-08-10 11:22:23 +0700

    up

--- sensors/src/Lidar.py
+++ sensors/src/Lidar.py
@@ -4,18 +4,11 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-    print('0:', msg.ranges[0]*100)
-    print('90:', msg.ranges[90]*100)
-    print('180:', msg.ranges[180]*100)
-    print('270:', msg.ranges[270]*100)
-    print('360:', msg.ranges[360]*100)
-    print('450:', msg.ranges[450]*100)
-    print('540:', msg.ranges[540]*100)
-    print('630:', msg.ranges[630]*100)
-    print('720:', msg.ranges[720]*100)
-    print('810:', msg.ranges[810]*100)
-    print('900:', msg.ranges[900]*100)
-    print('990:', msg.ranges[990]*100)
+
+    print(' Depan 585:', msg.ranges[585]*100)
+    print(' Kanan 270:', msg.ranges[270]*100)
+    print(' Belakang 0:', msg.ranges[0]*100)
+    print(' Kiri 855:', msg.ranges[855]*100)
 
 
 rospy.init_node('data_lidar')
