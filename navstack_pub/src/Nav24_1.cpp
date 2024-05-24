aa78bb02bc668f7aa262ec9f151d7d1fb5efc743
Alfin Fadilah Freitas <alfin160103@gmail.com>
2024-05-24 08:18:36 +0700

    update 2024

--- navstack_pub/src/Nav24_1.cpp
+++ navstack_pub/src/Nav24_1.cpp
@@ -17,24 +17,25 @@
 #include <termios.h>
 #include <map>
 
-float ping[6]={0,0,0,0,0,0};
-// kanan, depan, belakang, kiri, imu z, imu w
+float ping[7]={0,0,0,0,0,0,0};
+// depan, belakang, kiri, kanan, roll, pitch, yaw
 void tofdistancesCallback(const std_msgs::Int32MultiArray::ConstPtr& msg)
 {
   for (int i=0;i<4;i++){
     ping[i]=msg->data[i];
   }
-  ROS_INFO("I heard: [%f]", ping[0]);
+   ROS_INFO("ToF Reading: front=%f back=%f  left=%f right=%f", ping[0], ping[1], ping[2], ping[3]);
 }
 
 void eulerCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
-    // Extract Euler angles from the received message
-    ping[4] = msg->pose.position.z;     //yaw orientation
+  // Extract Euler angles from the received message
+  ping[4] = std::abs(msg->pose.position.x);     // roll orientation
+  ping[5] = std::abs(msg->pose.position.y);     // pitch orientation
+  ping[6] = std::abs(msg->pose.position.z);     // yaw orientation
 
-    ROS_INFO("IMU Orientation: yaw=%f", ping[4]);
+  ROS_INFO("IMU Orientation: roll=%f pitch=%f yaw=%f", ping[4], ping[5], ping[6]);
 }
-
 
 float xaa[5],yaa[5],xas[5];
 bool ff1,ff2,ff3;
@@ -86,30 +87,100 @@ std::map<char, std::vector<float>> moveBindings{
     {'C', {-1, 1, 0, 0}}};
 
 //step
-char a_gerak[]  ={'a','x','D','w','s'}
-// step            0   1   2   3   4   5   6   7   8   9  10   11  12  13  14  15  16  17  18  19  20  21  22  23 24   25  26  27  28  29  30  31  32  33  34  35  36 37  38   39  40  41  42 43  44   45  46  47  48  49  50  51  52  53  54  55  56  57
+char a_gerak[]  ={'d','d','w','a','w','s','s','x','d','w','w','w','A','w','d','w','s','x','x','d','w','s','s','x','d','w','D','w','s','x','a','w','D','w','A','w','w','d','w','A','a'};
+// step            0   1   2   3   4   5   6   7   8   9  10   11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30  31  32  33  34  35  36  37  38  39
 
-//program buat limit sensor dan gerakan kaki dan juga gerakan gripper
+// Pengondisian step dan batas gerakan
 std::map<int, std::vector<float>> step{
-  //{step, {Tof_Kanan, Tof_depan, Tof_belakang, Tof_Kiri, Imu Yaw, Gripper (lifter), Gripper (gripper), Speed, Turn}}
+  // {step, {Tof_depan, Tof_belakang, Tof_kiri, Tof_kanan, Imu Roll, Imu Pitch, Imu Yaw, Gripper (lifter), Gripper (gripper), Speed, Turn}}
   // gripper: teleop 'o' --> {0,0}; teleop 'p' --> {-2,0}; teleop 'l' --> {0,-1}; teleop ';' --> {-1,-1}
-  {0, {500,0,0,200,0,-2,0,1,2}},
-  {1, {0,0,60,0,0,-2,0,1,1}},
-  {2, {0,220,0,520,0,-2,0,2,1}}, //keluar home
-  {3, {0,0,190,0,0,0,-1,1,1}}, //ke k1
-  {4, {0,140,0,0,0,0,0,1,1}}, //k1
+  {0,   {0,0,0,0,0,0,20,            -2,0,1,1}},   // keluar dari home (handling pengondisian untuk orientasi berbeda)
+  {1,   {500,0,100,100,0,0,90,      -2,0,1,1}},   // keluar dari home
+  {2,   {0,520,220,0,0,0,0,         -2,0,2,1}},   // menuju zona K1
+  {3,   {250,125,470,500,0,0,5,   -2,0,1,1}},   // berotasi hingga gripper sejajar K1
+  {4,   {150,190,0,0,0,0,0,         0,-1,1,1}},   // mendekati K1 (gripper diturunkan dan terbuka) 
+  {5,   {140,0,0,0,0,0,0,            0,0,1,1}},   // di posisi K1 dan gripper men-grip korban
+  {6,   {140,0,0,0,0,0,0,           -2,0,1,1}},   // gripper dengan korban diangkat kembali
+  {7,   {0,100,0,0,0,0,0,           -2,0,1,1}},   // keluar dari zona K1 
+  {8,   {0,500,300,150,0,0,90,      -2,0,1,1}},   // berotasi sejajar jalur utama
+  {9,   {0,580,0,0,0,0,0,           -2,0,2,1}},   // bergerak maju hingga ke R1 (Jalan Retak)
+  {10,  {0,0,0,0,0,10,0,            -2,0,2,1}},   // bergerak maju di R1 (Jalan Retak)
+  {11,  {300,0,0,0,0,0,0,           -2,0,2,1}},   // bergerak maju melewati R2 (Turunan) dan R3 (Bebatuan) - Menuju SZ1
+  {12,  {0,0,70,0,0,0,0,            -2,0,1,1}},   // menyamping ke kiri sebelum ke SZ1
+  {13,  {100,0,400,0,0,0,0,         -2,0,1,1}},   // maju dan bersiap ke SZ1
+  {14,  {0,0,120,0,0,0,175,         -2,0,1,1}},   // berotasi hingga sejajar dengan SZ1
+  {15,  {220,500,0,0,0,0,0,         -2,0,1,1}},   // mendekat pada SZ1
+  {16,  {100,0,0,0,0,0,0,           0,-1,1,1}},   // menurunkan K1 di SZ1
+  {17,  {0,500,0,0,0,0,0,           0,-1,1,1}},   // mundur dengan gripper masih terbuka (handling korban terangkat lagi)
+  {18,  {0,430,0,0,0,0,0,           -2,0,1,1}},   // mundur hingga tegak lurus K2
+  {19,  {290,0,0,280,0,0,90,       -2,0,1,1}},   // berotasi hingga gripper sejajar K2
+  {20,  {110,180,0,0,0,0,         0,-1,1,1}},   // mendekati K2 (gripper diturunkan dan terbuka) 
+  {21,  {110,0,0,0,0,0,            0,0,1,1}},   // di posisi K2 dan gripper men-grip korban
+  {22,  {120,0,0,0,0,0,           -2,0,1,1}},   // gripper dengan korban diangkat kembali
+  {23,  {0,70,0,0,0,0,            -2,0,1,1}},   // bergerak mundur
+  {24,  {0,400,0,50,0,0,          -2,0,1,1}},   // berotasi hingga sejajar dengan SZ2
+  {25,  {0,200,0,0,0,0,0,           -2,0,1,1}},   // maju menuju SZ2
+  {26,  {0,0,0,50,0,0,5,            -2,0,1,1}},   // menyamping mendekati tembok agar sejajar dengan SZ2
+  {27,  {0,170,0,0,0,0,0,           -2,0,1,1}},   // maju menuju SZ2
+  {28,  {100,0,0,0,0,0,0,           0,-1,1,1}},   // menurunkan K2 di SZ2
+  {29,  {0,500,0,0,0,0,0,           0,-1,1,1}},   // mundur dengan gripper masih terbuka (handling korban terangkat lagi)
+  {30,  {0,0,0,0,0,0,90,            -2,0,1,1}},   // berotasi hingga tegak lurus SZ2
+  {31,  {0,250,370,0,0,0,0,         -2,0,1,1}},   // maju keluar SZ2
+  {32,  {0,0,0,80,0,0,0,            -2,0,1,1}},   // menyamping keluar SZ2
+  {33,  {0,500,300,0,0,0,0,         -2,0,1,1}},   // maju keluar SZ2
+  {34,  {0,0,100,0,0,0,0,           -2,0,1,1}},   // menyamping memposisikan diri menuju R? (Jalan Retak)
+  {35,  {0,350,0,0,0,0,0,           -2,0,1,1}},   // maju menuju R? (Jalan Retak)
+  {36,  {100,500,0,0,0,0,0,         -2,0,1,1}},   // maju melewati R? (Jalan Retak) dan R? (Bebatuan)
+  {37,  {0,0,0,0,0,0,5,             -2,0,1,1}},   // berotasi memposisikan diri sejajar dengan tangga
+  {38,  {100,300,0,0,0,0,0,         -2,0,1,1}},   // maju hingga body robot collinear dengan tangga
+  {39,  {0,400,200,0,10,0,0,        -2,0,1,1}},   // melewati tangga
+  {40,  {0,0,240,300,0,0,90,       -2,0,1,1}},
 };
 
 std::map<int, std::vector<bool>> _f_{
-  // ini program untuk kondisi if 1 atau 0 (komparasi)
-  // komparator (0)(sensor>=batas) (1)(Sensor<=batas) (index 0 - 4)
-  // 
-  // uneven = (0, 1) && normal = (0, 0) (index 6 - 7) 
-  {0, {0,0,0,1,0,1,0,0}},
-  {1, {0,0,1,0,0,1,0,0}},
-  {2, {0,0,0,0,0,1,0,0}}, // posisi home gerak ke kanan semua sensor nilai lebih dari batas
-  {3, {0,0,0,0,0,1,0,0}},
-  {4, {0,1,0,0,0,1,0,0}},
+  // komparator (0)(sensor>=batas) (1)(Sensor<=batas) (index 0 - 7)
+  // uneven = (0, 1) && normal = (0, 0) (index 9 - 10) 
+  {0,   {0,0,0,0,0,0,0, 1,0,0}},
+  {1,   {0,0,1,1,0,0,0, 1,0,0}},
+  {2,   {0,0,0,0,0,0,0, 1,0,0}},
+  {3,   {1,1,0,0,0,0,1, 1,0,0}}, 
+  {4,   {1,0,0,0,0,0,0, 1,0,0}},
+  {5,   {1,0,0,0,0,0,0, 1,0,0}},
+  {6,   {0,0,0,0,0,0,0, 1,0,0}},
+  {7,   {0,1,0,0,0,0,0, 1,0,0}},
+  {8,   {0,0,1,1,0,0,0, 1,0,0}},
+  {9,   {0,0,0,0,0,0,0, 1,0,0}},
+  {10,  {0,0,0,0,0,0,0, 1,0,1}},
+  {11,  {1,0,0,0,0,0,0, 1,0,0}},
+  {12,  {0,0,1,0,0,0,0, 1,0,0}},
+  {13,  {1,0,0,0,0,0,0, 1,0,0}},
+  {14,  {0,0,1,0,0,0,0, 1,0,0}},
+  {15,  {1,0,0,0,0,0,0, 1,0,0}},
+  {16,  {1,0,0,0,0,0,0, 1,0,0}},
+  {17,  {0,1,0,0,0,0,0, 1,0,0}},
+  {18,  {0,1,0,0,0,0,0, 1,0,0}},
+  {19,  {1,0,0,0,0,0,0, 1,0,0}},
+  {20,  {1,0,0,0,0,0,0, 1,0,0}},
+  {21,  {1,0,0,0,0,0,0, 1,0,0}},
+  {22,  {0,0,0,0,0,0,0, 1,0,0}},
+  {23,  {0,1,0,0,0,0,0, 1,0,0}},
+  {24,  {0,0,0,1,0,0,1, 1,0,0}},
+  {25,  {0,1,0,0,0,0,1, 1,0,0}},
+  {26,  {0,0,0,1,0,0,1, 1,0,0}},
+  {27,  {0,1,0,0,0,0,1, 1,0,0}},
+  {28,  {1,0,0,0,0,0,0, 1,0,0}},
+  {29,  {0,1,0,0,0,0,0, 1,0,0}},
+  {30,  {0,0,0,0,0,0,0, 1,0,0}},
+  {31,  {0,0,1,0,0,0,0, 1,0,0}},
+  {32,  {0,0,0,1,0,0,0, 1,0,0}},
+  {33,  {0,0,0,0,0,0,0, 1,0,0}},
+  {34,  {0,0,1,0,0,0,0, 1,0,0}},
+  {35,  {0,0,0,0,0,0,0, 1,0,0}},
+  {36,  {1,0,0,0,0,0,0, 1,0,1}},
+  {37,  {0,0,0,0,0,0,1, 1,0,1}},
+  {38,  {1,0,0,0,0,0,0, 1,0,1}},
+  {39,  {0,0,1,0,1,0,0, 1,0,1}},
+  {40,  {0,0,0,1,0,0,0, 1,0,1}},
 };
 
 // Init variables
@@ -190,33 +261,122 @@ void kontrol(char arah_, int step_){
   if (isAvoidanceActive) {
     return;
   }
-  key=arah_;
-  int batas[5];
+  if (step_ == 0)
+  {
+    // Conditioning for robot first orientation
+    if(ping[3] > ping[2])         // if the right's measurement greater than left's
+    {
+      key = arah_;
+      diffOrient = false;
+    }
+    else if(ping[3] > ping[2])    // if the right's measurement smaller than left's
+    {
+      key = 'a';
+      diffOrient = true;
+    }
+  }
+  else if (step_ == 1 && diffOrient)
+  {
+    key = 'a';
+  }
+  else
+  {
+    key = arah_;
+  }
+  int batas[7];
   if (step.count(step_) == 1)
     {
-      for(int a=0;a<5;a++){
+      for(int a=0;a<7;a++){
         batas[a]=step[step_][a];
+        if (diffOrient)
+        {
+          if (step_ == 3)
+          {
+            batas[6] = 175;
+          }
+          else if (step_ == 14)
+          {
+            batas[6] = 5;
+          }
+          else if (step_ == 24)
+          {
+            batas[6] = 175;
+          }
+          else if (step_ == 37)
+          {
+            batas[6] = 175;
+          }
+        }
       }
       // currentStep = step_;
-      xb=step[step_][5];
-      yb=step[step_][6];
-      speed=step[step_][7];
-      turn=step[step_][8];
+      xb=step[step_][7];
+      yb=step[step_][8];
+      speed=step[step_][9];
+      turn=step[step_][10];
     }
 
-  bool flag_[5];
+  bool flag_[7];
   if (_f_.count(step_) == 1)
     {
-      for(int a=0;a<5;a++){
+      for(int a=0;a<7;a++){
         flag_[a]=_f_[step_][a];
+        if (diffOrient)
+        {
+          if (step_ == 3)
+          {
+            flag_[6] = 0;
+          }
+          else if (step_ == 8)
+          {
+            flag_[6] = 1;
+          }
+          else if (step_ == 14)
+          {
+            flag_[6] = 1;
+          }
+          else if (step_ == 19)
+          {
+            flag_[6] = 0;
+          }
+          else if (step_ == 24)
+          {
+            flag_[6] = 0;
+          }
+          else if (step_ == 30)
+          {
+            flag_[6] = 1;
+          }
+          else if (step_ == 37)
+          {
+            flag_[6] = 0;
+          }
+          else if (step_ == 40)
+          {
+            flag_[6] = 0;
+          }
+        }
       }
-    pilih=_f_[step_][5];
-    imu_override_.data = _f_[step_][6];
-    leg_height_.data = _f_[step_][7];
+    pilih=_f_[step_][7];
+    imu_override_.data = _f_[step_][8];
+    leg_height_.data = _f_[step_][9];
     }
     
 
-  if (moveBindings.count(key) == 1)
+  // Trying to stabilize the orientation of robot's body
+  if (moveBindings.count(key) == 1 && (step_ == 10 || step_ == 36))
+  {
+    float desired_yaw = 90.0;
+    float yaw_difference = desired_yaw - ping[6];
+    if (std::abs(yaw_difference) > 5.0) 
+    {
+      // If the yaw difference is greater than 5 degrees, rotate until desired orientation is reached
+      twist.angular.z = (yaw_difference > 0) ? 0.5 : -0.5; // Adjust the angular velocity as needed
+      // Keep linear velocities zero
+      twist.linear.x = 0;
+      twist.linear.y = 0;
+      twist.linear.z = 0;
+    }
+    else
     {
       // Grab the direction data
       x = moveBindings[key][0];
@@ -227,6 +387,21 @@ void kontrol(char arah_, int step_){
            
       ROS_INFO("\rCurrent: speed %f   | turn %f | Last command: %c   ", speed, turn, key);
     }
+  }
+  else
+  {
+    if (moveBindings.count(key) == 1)
+    {
+      // Grab the direction data
+      x = moveBindings[key][0];
+      y = moveBindings[key][1];
+      z = moveBindings[key][2];
+      th = moveBindings[key][3];
+      imu_override_.data = false;
+           
+      ROS_INFO("\rCurrent: speed %f   | turn %f | Last command: %c   ", speed, turn, key);
+    }
+  }
 
     // Update the Twist message
     twist.linear.x = x * speed *0.5;
@@ -244,16 +419,16 @@ void kontrol(char arah_, int step_){
     Led_.data=2;
     
   
-    ROS_INFO("%d, %d, %d, %d ", batas[0], batas[1], batas[2], batas[3]);
-    ROS_INFO("%f, %f, %f, %f, %f, %f",ping[0],ping[1],ping[2],ping[3],ping[4],ping[5]);
-    ROS_INFO("%d, %d, %d, %d",flag_[0],flag_[1],flag_[2],flag_[3]);
+    ROS_INFO("%d, %d, %d, %d, %d, %d, %d ", batas[0], batas[1], batas[2], batas[3], batas[4], batas[5], batas[6]);
+    ROS_INFO("%f, %f, %f, %f, %f, %f, %f",ping[0],ping[1],ping[2],ping[3],ping[4],ping[5],ping[6]);
+    ROS_INFO("%d, %d, %d, %d, %d, %d, %d",flag_[0],flag_[1],flag_[2],flag_[3],flag_[4],flag_[5],flag_[6]);
 
 
     bool s[6]={false,false,false,false,false,false};
 
   if(pilih==true){
-    for (int a=0; a<5; a++){
-    for (int a = 0; a < 5; a++) {
+    for (int a=0; a<7; a++){
+    for (int a = 0; a < 7; a++) {
         if (flag_[a] == true) {
             if (ping[a] <= (batas[a] + offset) || ping[a] <= (batas[a] - offset)) {
                 s[a] = true;
@@ -273,7 +448,7 @@ void kontrol(char arah_, int step_){
 }
   else{
 
-    for (int a=0; a<5; a++){
+    for (int a=0; a<7; a++){
       xas[a]=xaa[a]-yaa[a];
       if(flag_[a]==true){
         if(xas[a]<=batas[a])
@@ -293,7 +468,7 @@ void kontrol(char arah_, int step_){
   }
 //  //ROS_INFO("%d, %d, %d, %d ",s[0], s[1], s[2], s[3], s[4]);
   
-  if(s[0]==true && s[1]==true && s[2]==true && s[3]==true && s[4]==true && s[5]==true){
+  if(s[0]==true && s[1]==true && s[2]==true && s[3]==true && s[4]==true && s[5]==true && s[6]==true){
     flag1++;
     ROS_INFO("clear");
     yaa[0]=xaa[0];
@@ -317,7 +492,7 @@ int main(int argc, char **argv)
   ros::Publisher state_pub_ = n.advertise<std_msgs::Bool>("/state", 100);
   ros::Publisher Led = n.advertise<std_msgs::Int32>("/led_control", 10);
   ros::Subscriber euler_sub = n.subscribe("/euler_topic", 10, eulerCallback);
-  ros::Subscriber sub = n.subscribe("tof_distances", 10, tofdistancesCallback);
+  ros::Subscriber sub = n.subscribe("/tof_distances", 10, tofdistancesCallback);
 
   ros::Subscriber _sub1 = n.subscribe("/chatter1", 1, chatter1Callback);
   ros::Subscriber _sub2 = n.subscribe("/chatter2", 1, chatter2Callback);
